aa627efb565a48554d619eb702fa6775e7cda94a
SlimGabsi <slimgabsi287@gmail.com>
2021-12-14 21:25:08 +0100

    Updates

---
+++ precache-manifest.12757f23183373965ba44ad4ee12da33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5d7ee6d78ded5f8bda6",
+    "url": "/reactnd-project-myreads/static/css/main.342b97b2.chunk.css"
+  },
+  {
+    "revision": "b5d7ee6d78ded5f8bda6",
+    "url": "/reactnd-project-myreads/static/js/main.b5d7ee6d.chunk.js"
+  },
+  {
+    "revision": "a8ab7a83a19cc4a52ba6",
+    "url": "/reactnd-project-myreads/static/js/1.a8ab7a83.chunk.js"
+  },
+  {
+    "revision": "d2f8cf40e53ea2d3746f",
+    "url": "/reactnd-project-myreads/static/js/runtime~main.d2f8cf40.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/reactnd-project-myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/reactnd-project-myreads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/reactnd-project-myreads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "1ee470f3a66d27ceb54cb4bf9a03de54",
+    "url": "/reactnd-project-myreads/index.html"
+  }
+];
